c5cb3a477f7c3f093406bf110ff3c79fa7093a10
Victor PINQUIER <vic.pinquier@orange.fr>
2019-03-04 16:04:06 +0100

    MQTT est là

--- src/testMQTT.java
+++ src/testMQTT.java
@@ -1,35 +1,45 @@
-import org.eclipse.paho.client.mqttv3.MqttClient;
-import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
-import org.eclipse.paho.client.mqttv3.MqttException;
-import org.eclipse.paho.client.mqttv3.MqttMessage;
+import org.eclipse.paho.client.mqttv3.*;
 import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
+import org.json.*;
 
-public class testMQTT {
+public class testMQTT implements MqttCallback{
+
+    private MqttClient client;
+    private static final String CONNECTION_URL = "tcp://neocampus.univ-tlse3.fr:1883";
+    private static final String SUBSCRIPTION = "u4/302/#";
+    private static final String USERNAME = "m2dc";
+    private static final String PASSWORD = "m2dc;18";
+
+    public testMQTT() {
+    }
 
     public static void main(String[] args) {
+        new testMQTT().run();
+    }
 
-        String topic        = "MQTT Examples";
-        String content      = "Message from MqttPublishSample";
-        int qos             = 2;
-        String broker       = "tcp://iot.eclipse.org:1883";
+    public void run() {
+
         String clientId     = "JavaSample";
         MemoryPersistence persistence = new MemoryPersistence();
 
         try {
-            MqttClient sampleClient = new MqttClient(broker, clientId, persistence);
+            client = new MqttClient(CONNECTION_URL, clientId, persistence);
             MqttConnectOptions connOpts = new MqttConnectOptions();
+
+            connOpts.setUserName(USERNAME);
+            connOpts.setPassword(PASSWORD.toCharArray());
+
             connOpts.setCleanSession(true);
-            System.out.println("Connecting to broker: "+broker);
-            sampleClient.connect(connOpts);
+            System.out.println("Connecting to broker: "+CONNECTION_URL);
+            client.connect(connOpts);
             System.out.println("Connected");
-            System.out.println("Publishing message: "+content);
-            MqttMessage message = new MqttMessage(content.getBytes());
-            message.setQos(qos);
-            sampleClient.publish(topic, message);
-            System.out.println("Message published");
-            sampleClient.disconnect();
-            System.out.println("Disconnected");
-            System.exit(0);
+            client.setCallback(this);
+
+            client.subscribe(SUBSCRIPTION);
+
+            //client.disconnect();
+            //System.out.println("Disconnected");
+            //System.exit(0);
         } catch(MqttException me) {
             System.out.println("reason "+me.getReasonCode());
             System.out.println("msg "+me.getMessage());
@@ -38,5 +48,23 @@ public class testMQTT {
             System.out.println("excep "+me);
             me.printStackTrace();
         }
+    }
+
+    @Override
+    public void connectionLost(Throwable throwable) {
+
+    }
+
+    @Override
+    public void messageArrived(String s, MqttMessage mqttMessage) throws Exception {
+        //System.out.println(mqttMessage);
+        JSONObject obj = new JSONObject(mqttMessage.toString());
+        System.out.println(obj.toString());
+        System.out.println(obj.getString("subID"));
+    }
+
+    @Override
+    public void deliveryComplete(IMqttDeliveryToken iMqttDeliveryToken) {
+
     }
 }
