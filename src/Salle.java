6a035dec0c82409a3e63df37d7fc3232aafd7f68
Michael Geraedts-Muse <yononaka0@gmail.com>
2019-03-04 14:29:24 +0100

    test

--- src/Salle.java
+++ src/Salle.java
@@ -24,14 +24,14 @@ public class Salle extends Environment {
         // Ajouter tous les capteurs de la salle
     }
 
-    public List<Capteur> getCapteurs(Metrique metrique) {
+    /*public List<Capteur> getCapteurs(Metrique metrique) {
         // Met la liste dispositifs sous forme de stream, filtre pour ne garder que les capteur de la metrique
         // utilise un map pour cast le résultat en capteur
         // collect transforme le stream en liste
         return dispositifs.stream().filter((d) -> d instanceof Capteur && d.getMetrique() == metrique)
                 .map((d) -> (Capteur) d)
                 .collect(Collectors.toList());
-    }
+    }*/
 
     public List<Effecteur> getEffecteurs(Metrique metrique) {
         // Met la liste dispositifs sous forme de stream, filtre pour ne garder que les effecteurs de la metrique
