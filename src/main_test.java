861ee76c5d68d5c555efd66ad6ad39e7446d1bac
knightdestin <maxnoire@gmail.com>
2019-03-11 14:42:07 +0100

    Suppression des fichiers inutiles

--- src/main_test.java
+++
@@ -1,9 +0,0 @@
-import AMAS.AmasNeoCampus;
-import AMAS.SalleEnv;
-
-public class main_test {
-    public static void main(String[] args) {
-        SalleEnv campusfab = new SalleEnv("u4", "campusfab");
-        new AmasNeoCampus(campusfab);
-    }
-}
