e4a759055f0770ca1157cf17160d099fff618870
Sablayrolles <louis.sablayrolles@gmail.com>
2019-03-11 13:04:15 +0100

    ajout interface agent mqtt lumen ext

--- src/InterfaceAgentCapteur.java
+++ src/InterfaceAgentCapteur.java
@@ -4,12 +4,12 @@ public class InterfaceAgentCapteur {
     private MQTTInterfaceCapteur inter;
 
     public InterfaceAgentCapteur(ArrayList<String> types) {
-        /* types = ["presence", "luminosite"]*/
+        /* types = ["presence", "luminosite", "luminosite_ext"]*/
         this.inter = new MQTTInterfaceCapteur(types);
     }
 
     public boolean getPresence(){
-        boolean presence:
+        boolean presence = false;
 
         for(float i : inter.getValuesOfType("presence")){
             presence = presence || (i>0.0);
@@ -23,6 +23,24 @@ public class InterfaceAgentCapteur {
         int s = 0;
 
         for(float i : inter.getValuesOfType("luminosite")){
+            if(i > 0) {
+                m += i;
+                s++;
+            }
+        }
+
+        if(s == 0) {
+            return m / (float) s;
+        }else{
+            return -1;
+        }
+    }
+
+    public float getLuminositeInterieur(){
+        float m = 0;
+        int s = 0;
+
+        for(float i : inter.getValuesOfType("luminosite_ext")){
             if(i > 0) {
                 m += i;
                 s++;
