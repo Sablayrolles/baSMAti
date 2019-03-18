a33360d20a4f0d27af05b216359e3e4ebd187dfc
Michael Geraedts-Muse <yononaka0@gmail.com>
2019-03-18 17:39:30 +0100

    rajout d'agents multiples (3lum, 3volet)

--- src/AMAS/AmasNeoCampus.java
+++ src/AMAS/AmasNeoCampus.java
@@ -4,16 +4,24 @@ import fr.irit.smac.amak.Amas;
 import fr.irit.smac.amak.Scheduling;
 
 public class AmasNeoCampus extends Amas<SalleEnv> {
-    AgentLumiere agentLumiere;
-    AgentVolet agentVolet;
+    AgentLumiere agentLumiere1;
+    AgentLumiere agentLumiere2;
+    AgentLumiere agentLumiere3;
+    AgentVolet agentVolet1;
+    AgentVolet agentVolet2;
+    AgentVolet agentVolet3;
     public AmasNeoCampus(SalleEnv environment) {
         super(environment, Scheduling.DEFAULT);
     }
 
     @Override
     protected void onInitialAgentsCreation() {
-        agentLumiere = new AgentLumiere(this);
-        agentVolet = new AgentVolet(this);
+        agentLumiere1 = new AgentLumiere(this);
+        agentVolet1 = new AgentVolet(this);
+        agentLumiere2 = new AgentLumiere(this);
+        agentVolet2 = new AgentVolet(this);
+        agentLumiere3 = new AgentLumiere(this);
+        agentVolet3 = new AgentVolet(this);
         // TODO initaliser les agents?
 
 
