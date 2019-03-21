ef44342ff281ca148452316277a15f0ec7e4cc53
Victor PINQUIER <vic.pinquier@orange.fr>
2019-03-21 13:48:36 +0100

    MQTT : prise en compte des commandes effecteur

--- src/Enumerations/Constantes.java
+++ src/Enumerations/Constantes.java
@@ -8,5 +8,19 @@ public final class Constantes {
     public static final int SEUIL_LUM_EXT = 25000;
     public static final int SEUIL_LUM_INT = 250;
     public static final int AMAS_TICKRATE_MILLISECOND = 5000;
+
+    // Commandes MQTT
+
+    // Volets
+    public static final String TOPIC_VOLETS = "u4/302/shutter";
+    public static final String COMMANDE_VOLETS_STATUS = "{\"dest\":\"all\",\"order\":\"status\"}";
+    public static final String COMMANDE_VOLETS_UP = "{\"dest\":\"all\",\"order\":\"up\"}";
+    public static final String COMMANDE_VOLETS_DOWN = "{\"dest\":\"all\",\"order\":\"down\"}";
+
+    // Lumières
+    public static final String TOPIC_LUMIERES = "u4/302/lighting";
+    public static final String COMMANDE_LUMIERE_STATUS = "{\"dest\":\"all\",\"order\":\"status\"}";
+    public static final String COMMANDE_LUMIERE_ON = "{\"dest\":\"all\",\"order\":\"on\"}";
+    public static final String COMMANDE_LUMIERE_OFF = "{\"dest\":\"all\",\"order\":\"off\"}";
 }
 
